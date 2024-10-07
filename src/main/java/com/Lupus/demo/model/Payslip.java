16dc7e277bd492e2d8ad3a5c03ac7a9eeaeea90f
Gabriel Stachnik <63073744+Celiek@users.noreply.github.com>
2024-10-07 23:45:28 +0200

    Basic Entities

--- src/main/java/com/Lupus/demo/model/Payslip.java
+++ src/main/java/com/Lupus/demo/model/Payslip.java
@@ -1,7 +1,33 @@
 package com.Lupus.demo.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "wyplaty")
 public class Payslip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_pracownika", nullable = false)
+    private User user;
+
+    @Column(name = "zaliczki", precision = 10, scale = 2)
+    private BigDecimal zaliczki;
+
+    @Column(name = "wyplaty_tygodniowe", precision = 10, scale = 2)
+    private BigDecimal wyplatyTygodniowe;
+
+    @Column(name = "wyplata_miesieczna", precision = 10, scale = 2)
+    private BigDecimal wyplataMiesieczna;
+
 }
--- src/main/java/com/Lupus/demo/model/User.java
+++ src/main/java/com/Lupus/demo/model/User.java
@@ -1,15 +1,27 @@
 package com.Lupus.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Lob;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "pracownik")
+@Data
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id_pracownika;
     private String Imie;
     private String Nazwisko;
     private String Typ_pracownika;
     @Lob
+    @Column(name = "zdjecie")
     private byte[] zdjecie;
+    @OneToMany(mappedBy = "pracownik", cascade = CascadeType.ALL)
+    private List<WorkHours> workHoursUser;
 }
